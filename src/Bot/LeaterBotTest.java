c768ec842d2bf13cd44eb45381f05d377a4156dc
makosenkov <makosenkov@gmail.com>
2018-08-27 02:58:29 +0300

    тесты по всем картам, удручающая статистика 21/28 пройденных остальные крашатся по IndexOutOfBounds без каких-либо объяснений

--- src/Bot/LeaterBotTest.java
+++ src/Bot/LeaterBotTest.java
@@ -58,21 +58,116 @@ public class LeaterBotTest {
     }
 
     @Test
-    public void ems1() {
-        testBotOnMap("maps/ems1.map", "ems1", 0, 377);
+    public void beard4() {
+        testBotOnMap("maps/beard4.map", "beard4", 0, 0);
+    }
+
+    @Test
+    public void beard5() {
+        testBotOnMap("maps/beard5.map", "beard4", 0, 0);
+    }
+
+    @Test
+    public void contest1() {
+        testBotOnMap("maps/contest1.map", "contest1", 0, 0);
+    }
+
+    @Test
+    public void contest2() {
+        testBotOnMap("maps/contest2.map", "contest2", 0, 0);
     }
 
     @Test
     public void contest3() {
         testBotOnMap("maps/contest3.map", "contest3", 0, 524);
     }
+    
+    @Test
+    public void contest4() {
+        testBotOnMap("maps/contest4.map", "contest4", 0, 0);
+    }
 
+    @Test
+    public void contest5() {
+        testBotOnMap("maps/contest5.map", "contest5", 0, 0);
+    }
+    
     @Test
     //Не ест дальнюю лямбду, а надо бы
     public void contest6() {
         testBotOnMap("maps/contest6.map", "contest6", 0, 712);
     }
 
+    @Test
+    public void contest7() {
+        testBotOnMap("maps/contest7.map", "contest7", 0, 0);
+    }
+
+    @Test
+    public void contest8() {
+        testBotOnMap("maps/contest8.map", "contest8", 0, 0);
+    }
+
+    @Test
+    public void contest9() {
+        testBotOnMap("maps/contest9.map", "contest9", 0, 0);
+    }
+
+    @Test
+    public void contest10() {
+        testBotOnMap("maps/contest10.map", "contest10", 0, 0);
+    }
+    
+    @Test
+    public void custom0() {
+        testBotOnMap("maps/custom0.map", "custom0", 0, 0);
+    }
+    
+    @Test
+    public void ems1() {
+        testBotOnMap("maps/ems1.map", "ems1", 0, 377);
+    }
+
+    @Test
+    public void flood1() {
+        testBotOnMap("maps/flood1.map", "flood1", 0, 0);
+    }
+
+    @Test
+    public void flood2() {
+        testBotOnMap("maps/flood2.map", "flood2", 0, 0);
+    }
+
+    @Test
+    public void flood3() {
+        testBotOnMap("maps/flood3.map", "flood3", 0, 0);
+    }
+
+    @Test
+    public void flood4() {
+        testBotOnMap("maps/flood4.map", "flood4", 0, 0);
+    }
+
+    @Test
+    public void flood5() {
+        testBotOnMap("maps/flood5.map", "flood5", 0, 0);
+    }
+
+    @Test
+    public void trampoline1() {
+        testBotOnMap("maps/trampoline1.map", "trampoline1", 0, 0);
+    }
+
+    @Test
+    public void trampoline2() {
+        testBotOnMap("maps/trampoline2.map", "trampoline2", 0, 0);
+    }
+
+    @Test
+    public void trampoline3() {
+        testBotOnMap("maps/trampoline3.map", "trampoline3", 0, 0);
+    }
+    
     @Test
     public void horock2() {
         testBotOnMap("maps/horock2.map", "horock2", 0, 1203);
