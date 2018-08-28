bf1bc55e32a9fb8f87c3842c08f1b49d3962cadb
JakeIlan <pbpn2@mail.ru>
2018-08-28 04:45:05 +0300

    -потестил результаты тестов на кошерном симуляторе, прокомментил тесты

--- src/Bot/LeaterBotTest.java
+++ src/Bot/LeaterBotTest.java
@@ -39,133 +39,133 @@ public class LeaterBotTest {
 
     }
 
-    @Test
+    @Test //ЭТО ВООБЩЕ ХУИТА КАКАЯ-ТО, ТУТ БОРОД И БРИТВ НЕТ
     public void beard0() {
         testBotOnMap("maps/beard0.map", "beard0", 0, 906);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void beard1() {
         testBotOnMap("maps/beard1.map", "beard1", 0, 891);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void beard2() {
         testBotOnMap("maps/beard2.map", "beard2", 0, 2812);
     }
 
-    @Test
+    @Test //TODO технически проходит, но почему-то абортится в двух шагах от лямбды
     public void beard3() {
         testBotOnMap("maps/beard3.map", "beard3", 0, 954);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void beard4() {
         testBotOnMap("maps/beard4.map", "beard4", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void beard5() {
         testBotOnMap("maps/beard5.map", "beard5", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED только там очков на 100 меньше должно быть
     public void contest1() {
         testBotOnMap("maps/contest1.map", "contest1", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void contest2() {
         testBotOnMap("maps/contest2.map", "contest2", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED, опять себе лишних очков нарисовал
     public void contest3() {
         testBotOnMap("maps/contest3.map", "contest3", 0, 524);
     }
     
-    @Test
+    @Test //CONFIRMED, а тут наоборот, меньше дал, чем на самом деле, скромняга
     public void contest4() {
         testBotOnMap("maps/contest4.map", "contest4", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void contest5() {
         testBotOnMap("maps/contest5.map", "contest5", 0, 0);
     }
     
-    @Test
+    @Test //CONFIRMED
     //Не ест дальнюю лямбду, а надо бы
     public void contest6() {
         testBotOnMap("maps/contest6.map", "contest6", 0, 712);
     }
 
-    @Test
+    @Test //CONFIRMED, BUT STILL WRONG SCORE (DANEK PRIVET)
     public void contest7() {
         testBotOnMap("maps/contest7.map", "contest7", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void contest8() {
         testBotOnMap("maps/contest8.map", "contest8", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED, НО ОН КАК-ТО ТУПО СДЕЛАЛ (НО ОН ВСЕ РАВНО КРУТОЙ)
     public void contest9() {
         testBotOnMap("maps/contest9.map", "contest9", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void contest10() {
         testBotOnMap("maps/contest10.map", "contest10", 0, 0);
     }
     
-    @Test
+    @Test //CONFIRMED, OPYAT OCHKI SCHITAET NE TAK, A MNE LEN' MENYAT' RASKLADKU
     public void ems1() {
         testBotOnMap("maps/ems1.map", "ems1", 0, 377);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void flood1() {
         testBotOnMap("maps/flood1.map", "flood1", 0, 0);
     }
 
-    @Test
+    @Test //TODO а тут робот сдох (утонул лох)
     public void flood2() {
         testBotOnMap("maps/flood2.map", "flood2", 0, 0);
     }
 
-    @Test
+    @Test //TODO опять утонул лох
     public void flood3() {
         testBotOnMap("maps/flood3.map", "flood3", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED NE LOX
     public void flood4() {
         testBotOnMap("maps/flood4.map", "flood4", 0, 0);
     }
 
-    @Test
+    @Test //TODO снова лох, чето с водой походу, он в ней вообще на похуй ходит
     public void flood5() {
         testBotOnMap("maps/flood5.map", "flood5", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void trampoline1() {
         testBotOnMap("maps/trampoline1.map", "trampoline1", 0, 0);
     }
 
-    @Test
+    @Test //TODO технически, соответствует оригинальному симулятору, но что-то странное со сбором лямбд, мог бы и выйти в WIN
     public void trampoline2() {
         testBotOnMap("maps/trampoline2.map", "trampoline2", 0, 0);
     }
 
-    @Test
+    @Test //CONFIRMED
     public void trampoline3() {
         testBotOnMap("maps/trampoline3.map", "trampoline3", 0, 0);
     }
     
-    @Test
+    @Test //TODO говорю же, что-то с водой
     public void horock2() {
         testBotOnMap("maps/horock2.map", "horock2", 0, 1203);
     }
@@ -173,7 +173,7 @@ public class LeaterBotTest {
 
     //TODO в нормальном симуляторе при текущей выходной последовательности бот ломается под камнем
     //TODO Проверить этот тест после отладки лямбда камней
-    @Test
+    @Test //CONFIRMED
     public void horock3() {
         testBotOnMap("maps/horock3.map", "horock3", 0, 1203);
     }
